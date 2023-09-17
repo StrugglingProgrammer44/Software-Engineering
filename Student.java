5a41a0a02ad817b7bb3e89d92ebb2b2fbdce8488
Jonathon <capej13@farmingdale.edu>
2023-09-16 23:02:55 -0400

    Student class

--- Student.java
+++ Student.java
@@ -4,19 +4,16 @@ public class Student {
 
     // Constructor
     public Student(String name, int age) {
-        // TODO: Complete the constructor logic
         this.name = name;
         this.age = age;
     }
     // Setter for name
     public void setName(String name) {
-        // TODO: Complete the setter logic
         this.name = name;
     }
 
     // Getter for name
     public String getName() {
-        // TODO: Complete the getter logic
         return name;
     }
 
